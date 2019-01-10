13ea98dc5128cd5d759aa21b17cc52ca15adebe0
Shiro <coleanhdasaipro97@gmail.com>
2019-01-10 14:17:24 +0700

    Add eat function in Cat.js and modified stuffs

--- Cat.js
+++ Cat.js
@@ -2,8 +2,11 @@ var Mouse = require('Mouse.js');
 
 function Cat() {
 	console.log("Mew");
-
+	this.stomach = [];
 	eat(mouse);
 }
  
+Cat.prototype.eat function (mouse) {
+	this.stomach.push(mouse);	
+}
 module.exports = Cat;
