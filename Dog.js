b98a50307da83f49a6c555ec66c575f8d2e81d76
Shiro <coleanhdasaipro97@gmail.com>
2019-01-13 13:20:34 +0700

    Modified Dog.js

--- Dog.js
+++ Dog.js
@@ -1,12 +1,13 @@
-function Dog() {
+var chalk = require('chalk');
+
+function Dog(name,weight) {
 	this.name = name;
 	this.stomach = [];
 	this.weight = weight;
-	
-	console.log("Goof");
 }
-Dog.prototype.eat = function(bone) {
-	this.stomach.push(bone);
+Dog.prototype.goof = function() {
+	console.log("Goof");
+	console.log("My name is "+chalk.blue(this.name) +"and i'm " +this.weight+" kg");
 };
 
 module.exports = Dog;
